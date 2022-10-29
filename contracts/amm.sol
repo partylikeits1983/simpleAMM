5b44ec7ea4a1609f59defd9f99cee3f68684517d
szabo <aleqvids@gmail.com>
2022-10-29 22:18:35 +0300

    fix

--- contracts/amm.sol
+++ contracts/amm.sol
@@ -150,7 +150,7 @@ contract AMM {
 		if(Pools[PID].token0 == tokenIn) {
 			// amount out Y
 			Pools[PID].amount0 += amount;
-			amountOut = int(amount).mul(int(Pools[PID].amount1)).div(int(amount).mul(int(Pools[PID].amount0)));
+			amountOut = int(amount).mul(int(Pools[PID].amount1)).div(int(amount + Pools[PID].amount0));
 
 			Pools[PID].amount1 -= uint(amountOut);
 
@@ -160,7 +160,7 @@ contract AMM {
 		else {
 			// amount out X
 			Pools[PID].amount1 += amount;
-			amountOut = int(amount).mul(int(Pools[PID].amount0)).div(int(amount).mul(int(Pools[PID].amount1)));
+			amountOut = int(amount).mul(int(Pools[PID].amount0)).div(int(amount + Pools[PID].amount1));
 
 			Pools[PID].amount0 -= uint(amountOut);
 
